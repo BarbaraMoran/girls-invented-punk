3d1afc996a5734dd9d47de9bb7966b5bbeeacea8
BarbaraMoran <barbaramoranrr@gmail.com>
2021-07-27 21:30:15 +0200

    add footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "../stylesheets/components/App.scss";
 import "../stylesheets/index.scss";
-import Header from "./Header";
-import Footer from "./Footer";
 import Landing from "./Landing/Landing";
 import MainGame2 from "./two-players/MainGame2";
 import MainGame1 from "./one-player/MainGame1";
+import MainPioneers from "./Pioneers/MainPioneers";
 import NotFound from "./NotFound";
 
 const App = () => {
   return (
     <>
-      <Header />
       <Switch>
         <Route exact path="/">
           <Landing> </Landing>
@@ -23,11 +21,13 @@ const App = () => {
         <Route path="/two-players">
           <MainGame2></MainGame2>
         </Route>
+        <Route path="/the-pioneers">
+          <MainPioneers></MainPioneers>
+        </Route>
         <Route>
           <NotFound></NotFound>
         </Route>
       </Switch>
-      <Footer />
     </>
   );
 };
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,30 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../stylesheets/components/Landing.scss";
 import videoMix from "../../images/mix.mp4";
+import videoMix2 from "../../images/background.mp4";
+import Header from "./Header";
 
 const Landing = () => {
   return (
-    <main className="landing__main">
-      <div className="main__container--title">
-        <h1 className="main__title">Girls invented Punk not England</h1>
-      </div>
-      <h2 className="main__subtitle">The Punk pioneers Game</h2>
-      <nav className="main__nav">
-        <div className="main__nav--container">
-          <Link className="button__player" to="/one-player">
-            One player
-          </Link>
+    <>
+      <Header />
+      <main className="landing__main">
+        <div className="main__container--title">
+          <h1 className="main__title">Girls invented Punk not England</h1>
         </div>
-        <div className="main__nav--container">
-          <Link className="button__player" to="/two-players">
-            Two players
-          </Link>
-        </div>
-      </nav>
-      <video className="main__background" autoPlay muted loop>
-        <source src={videoMix} type="video/mp4" />
-      </video>
-    </main>
+        <h2 className="main__subtitle">The Punk pioneers Game</h2>
+        <nav className="main__nav">
+          <div className="main__nav--container">
+            <Link className="button__player" to="/one-player">
+              One player
+            </Link>
+          </div>
+          <div className="main__nav--container">
+            <Link className="button__player" to="/two-players">
+              Two players
+            </Link>
+          </div>
+        </nav>
+        <video className="main__background" autoPlay muted loop>
+          <source src={videoMix} type="video/mp4" />
+        </video>
+        <video className="main__background-min" autoPlay muted loop>
+          <source src={videoMix2} type="video/mp4" />
+        </video>
+      </main>
+    </>
   );
 };
 
