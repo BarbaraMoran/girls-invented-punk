89b3de37226b2db62312ed7f3f6554d2d4cb3f02
BarbaraMoran <barbaramoranrr@gmail.com>
2021-08-18 14:15:28 +0200

    add react-bootstrap carousel

--- src/components/Pioneers/List.js
+++ src/components/Pioneers/List.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Artist from "./Artist";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const List = (props) => {
   const allArtists = props.pioneersData.map((eachPioneer) => {
-    return <Artist eachPioneer={eachPioneer} />;
+    return (
+      <Carousel.Item interval={30000}>
+        <Artist pioneer={eachPioneer} />
+      </Carousel.Item>
+    );
   });
 
-  return <ul className="pioneers__list">{allArtists}</ul>;
+  return (
+    <ul className="pioneers__list">
+      <Carousel variant="dark">{allArtists}</Carousel>
+    </ul>
+  );
 };
 export default List;
